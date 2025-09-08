ebdf1308f4ff5b2c275c34ad4cb516bcd09a2c30
Miguel S <m.angel.s.o@hotmail.com>
2025-09-08 17:02:14 -0500

    implementing the forward pass

--- src/convolutional_net.py
+++ src/convolutional_net.py
@@ -1,8 +1,25 @@
 import numpy as np
 
-def conv2d(image, kernel, padding=0, stride=1):
-    image = X_train[0]
-    padding=1
-    col_img, rows_img = image.shape
+def conv2d(data, kernel_size=1, filters=1, padding=0, stride=1):
+    rows_img, col_img = data.shape
     padded_img = np.zeros((rows_img+padding*2,col_img+padding*2))
-    padded_img[padding:image.shape[0]+padding, padding:image.shape[1]+padding] = image
+    padded_rows_img, padded_cols_img = padded_img.shape
+    padded_img[padding:padded_rows_img-padding, padding:padded_cols_img-padding] = data
+    
+    
+    kernels = np.random.normal(loc=0, scale=1, size=(kernel_size, kernel_size, filters))
+    bias = np.zeros
+    data_filtered = []
+    for kernel in kernels:
+        kernel_rows, kernel_cols = kernel.shape
+        img_filter_rows = padded_rows_img-kernel_rows+1
+        img_filter_cols = padded_cols_img-kernel_cols+1
+
+        image_filter = np.zeros((img_filter_rows, img_filter_cols))
+        
+        for i in range(img_filter_rows):
+            for j in range(img_filter_cols):
+                image_filter[i, j] = np.sum(padded_img[i:kernel_rows+i, j:kernel_cols+j]*kernel)
+
+
+    return data_filtered
