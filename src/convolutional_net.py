9d13d43872339a7b89812472eb4b114db0d2283b
Miguel S <m.angel.s.o@hotmail.com>
2025-09-08 23:20:50 -0500

    feat: The foward implementation in the conv layer is done

--- src/convolutional_net.py
+++ src/convolutional_net.py
@@ -1,25 +1,111 @@
 import numpy as np
-
-def conv2d(data, kernel_size=1, filters=1, padding=0, stride=1):
-    rows_img, col_img = data.shape
-    padded_img = np.zeros((rows_img+padding*2,col_img+padding*2))
-    padded_rows_img, padded_cols_img = padded_img.shape
-    padded_img[padding:padded_rows_img-padding, padding:padded_cols_img-padding] = data
+import abc
+class Network():
+    def __init__(self):
+        self.layers = []
+        self.cost_function = 0
+        self.optimizer = 0
     
+    def train():
+        pass
     
-    kernels = np.random.normal(loc=0, scale=1, size=(kernel_size, kernel_size, filters))
-    bias = np.zeros
-    data_filtered = []
-    for kernel in kernels:
-        kernel_rows, kernel_cols = kernel.shape
-        img_filter_rows = padded_rows_img-kernel_rows+1
-        img_filter_cols = padded_cols_img-kernel_cols+1
+    def predict():
+        pass
 
-        image_filter = np.zeros((img_filter_rows, img_filter_cols))
+class Layer(abc.ABC):
+    def __init__(self):
+        self.input = None
+        self.output = None
+    
+    @abc.abstractmethod
+    def forward(self, input):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def backward(self, output_gradient):
+        raise NotImplementedError
+    
+
+class ConvLayer(Layer):
+    def __init__(self, seed=42, data, kernel_size=1, filters=1, padding=0, stride=1):
+        super().__init__()
+        np.random.seed(seed)        
+        self.data = data
+        self.kernel_size = kernel_size
+        self.filters = filters
+        self.padding = padding
+        self.stride = stride
+        self.bias = np.zeros(filters)
+        self.output = None
+
+    def relu(self, z):
+        return np.maximun(0, z)
+    
+    def relu_derivate(self, z):
+        derivate = np.array(z>0, dtype=np.float64)
+        return derivate
         
-        for i in range(img_filter_rows):
-            for j in range(img_filter_cols):
-                image_filter[i, j] = np.sum(padded_img[i:kernel_rows+i, j:kernel_cols+j]*kernel)
+    def forward(self):
+        rows_img, col_img = self.data.shape
+        padded_img = np.zeros((rows_img+self.padding*2,col_img+self.padding*2))
+        padded_rows_img, padded_cols_img = padded_img.shape
+        padded_img[self.padding:padded_rows_img-self.padding, self.padding:padded_cols_img-self.padding] = self.data
+        
+        
+        kernels = np.random.normal(loc=0, scale=1, size=(self.kernel_size, self.kernel_size, self.filters))
+        data_filtered = []
+        for idx, kernel in enumerate(kernels):
+            kernel_rows, kernel_cols = kernel.shape
+            img_filter_rows = padded_rows_img-kernel_rows+1
+            img_filter_cols = padded_cols_img-kernel_cols+1
 
+            image_filter = np.zeros((img_filter_rows, img_filter_cols))
+            
+            for i in range(img_filter_rows):
+                for j in range(img_filter_cols):
+                    image_filter[i, j] = np.sum(padded_img[i:kernel_rows+i, j:kernel_cols+j]*kernel)+self.bias[idx]
+            
+            activated_filter = self.relu(image_filter)
+            data_filtered.append(activated_filter)
+        
+        self.output = np.array(data_filtered)
+        self.output = np.transpose(self.output, (1, 2, 0))
+        
+        return self.output
+        
+    def backward(self):
+        pass
 
-    return data_filtered
+class PoolingLayer(Layer):
+    def __init__(self, pool_size):
+        super().__init__()
+        self.pool_size = pool_size
+        
+    def forward(self):
+        pass
+                
+    def backward(self):
+        pass
+
+class FlattenLayer(Layer):
+    def __init__(self, input_shape):
+        super().__init__()
+        self.input_shape = input_shape
+        
+    def forward(self):
+        pass
+                
+    def backward(self):
+        pass
+
+class DenseLayer(Layer):
+    def __init__(self):
+        super().__init__()
+        self.weights = None
+        self.bias = None
+        
+    def forward(self):
+        pass
+                
+    def backward(self):
+        pass
